3c2a58c096dc819516d21c8dea4a0b8e901326c4
santhosh <santhosh.003z@gmail.com>
2025-03-14 00:29:13 +0530

    feat: added action buttons for files

--- public/scripts/vaultIndex.js
+++ public/scripts/vaultIndex.js
@@ -1,14 +1,20 @@
 const newFile = document.querySelector(`.new-file`);
-const modal = document.querySelector(`.newFile-modal`);
+const uploadModal = document.querySelector(`.newFile-modal`);
+const actionModal = document.querySelector(`.action-buttons-container`);
 const closeBtn = document.querySelector(`.close-modal`);
 const fileBtn = document.querySelector(`.browse-btn`);
+const actionBtn = document.querySelector(`.file-actions`);
 
 newFile.addEventListener(`click`, () => {
   fileBtn.value = ``;
-  modal.showModal();
+  uploadModal.showModal();
 });
 
 closeBtn.addEventListener(`click`, () => {
   fileBtn.value = ``;
-  modal.close();
+  uploadModal.close();
+});
+
+actionBtn.addEventListener(`click`, () => {
+  actionModal.classList.toggle(`action-hide`);
 });
